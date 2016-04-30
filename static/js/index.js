d9c1f26d8e17fac666be3c37c438f0c2fe01acfc
adyouri <raryat@gmail.com>
2016-04-30 16:54:48 +0100

    Fix javascript file indentation

--- static/js/index.js
+++ static/js/index.js
@@ -1,30 +1,33 @@
 // Send Http request
 function request(url,b,method,data,c){ 
- c = new XMLHttpRequest;
- c.open(method||'get',url);
- c.onload=b;
- c.send(data||null)
- s = document.getElementById("result");
- s.value = 'انتظر رجاء...'
+    c = new XMLHttpRequest;
+    c.open(method||'get',url);
+    c.onload = b;
+    c.send(data||null)
+    s = document.getElementById("result");
+    s.value = 'انتظر رجاء...'
  }
 
 function callback(e){
-	  s = document.getElementById("result");
-      short_link = window.location.host + JSON.parse(this.response).short_link 
-      // convert www.qssr.tk/jhFj => qssr.tk/jhFj
-      if (window.location.host.substring(0, 3) == 'www'){
-	  s.value = short_link.substring(4) 
-      }
-      
-      else {
-      s.value = short_link
-      }
+	s = document.getElementById("result");
+    short_link = window.location.host + JSON.parse(this.response).short_link 
+
+    // convert www.qssr.tk/jhFj => qssr.tk/jhFj
+    if (window.location.host.substring(0, 3) == 'www'){
+        s.value = short_link.substring(4) 
+    }
+    
+    else {
+        s.value = short_link
+    }
 }
+
 // On Change
 function change() {
-t = document.getElementById("input").value;
-request('sh?link=' + t, callback, 'post')
+    t = document.getElementById("input").value;
+    request('sh?link=' + t, callback, 'post')
 }
+
 // Copy to clipboard
 function cp() {
 	e = document.getElementById("result");
